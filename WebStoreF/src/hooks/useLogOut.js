88def1d3eb5f6d1074b803b1657c5cd7ef6cad31
DouglasWai2 <douglas.wai@outlook.com>
2024-02-27 09:28:07 -0300

    changed hardcoded url to environmental variable on logout

--- WebStoreF/src/hooks/useLogOut.js
+++ WebStoreF/src/hooks/useLogOut.js
@@ -2,20 +2,23 @@ import axios from "axios";
 
 const useLogOut = () => {
   async function logOut() {
-      try {
-        const data = await axios.get(`http://localhost:5000/api/v1/auth/logout`, {
+    try {
+      const data = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/v1/auth/logout`,
+        {
           withCredentials: true,
-        });
-        window.localStorage.removeItem("accessToken");
-        document.cookie = 'loggedin=True; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        window.localStorage.removeItem("role");
-        window.location.href = "/";
-      } catch (error) {
-        console.log(error);
-      }
-    
+        }
+      );
+      window.localStorage.removeItem("accessToken");
+      document.cookie =
+        "loggedin=True; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      window.localStorage.removeItem("role");
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error);
+    }
   }
-return logOut
-}
+  return logOut;
+};
 
-export { useLogOut }
+export { useLogOut };
